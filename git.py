bb404dafc4be98a39ba80c25f586517b34b9f682
fazliddinrahimov8081 <fazliddinrahimov8081@gmail.com>
2025-11-10 11:13:09 +0500

    amaliy

---
+++ git.py
@@ -0,0 +1,13 @@
+students = {
+	"Samandar": 18,
+	"Muzaffar": 19,
+	"Xojiakbar": 16,
+	"Islom": 20,
+	"Asomiddin": 14,
+	"Sobitjon": 17,
+	"Shoxruh": 20
+}
+
+for i in students:
+    if students [i] >= 18:
+       print( students[i], i)
